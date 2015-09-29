83a65491a85c09449010895226bb0e55d2166d3e
Charles <charlesbarbier@MacBook-Pro-de-Charles-2.local>
2015-09-29 12:05:57 +0200

    Socket OK

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 var express = require('express');
 var app = express();
-
 var http = require('http').Server(app);
-
-var fs = require('fs');
-var socket = require('socket.io');
-
-/*var page = null;
-fs.readFile('./index.html', function(err, data){
-  page = data;
-  console.log(page)
-});
-
-var server = http.createServer(function (req, res){
-  res.end('hello');
-});*/
+var fs = require('fs'); // not needed anymore
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendFile('index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
-server.listen(3000);
+app.get('/admin', function(req, res){
+  res.sendFile(__dirname + '/admin.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+  // send to front-office
+  socket.emit('news', 'welcome');
+
+  //listen to-back office
+  socket.on('message', function(body){
+    console.log(body);
+  });
+});
+
+http.listen(3000);
